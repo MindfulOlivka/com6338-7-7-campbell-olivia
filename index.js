6480f02a98ed1ed9234084f9455a95997e974c06
MindfulOlivka <o.campbell@ufl.edu>
2023-07-08 18:16:05 -0400

    quiz questions and var

---
+++ index.js
@@ -0,0 +1,67 @@
+var questionsArr = [
+    {
+        question: '1. What is the estimated age of the universe?',
+        answer: '13.8 billion years',
+        options: [
+            '13.8 billion years',
+            '4.6 billion years',
+            '10 million years',
+            '100 trillion years',
+        ]
+    },
+       {
+        question: '2. What is the name of the galaxy that contains our solar system?',
+        answer: 'Milky Way Galaxy',
+        options: [
+            'Andromeda Galaxy',
+            'Milky Way Galaxy',
+            'Orion Nebula',
+            'Sombrero Galaxy',
+        ]
+    },
+       {
+        question: '3. What is the force that opposes gravity and causes the expansion of the universe to accelerate?',
+        answer: 'Dark energy',
+        options: [
+            'Electromagnetic force',
+            'Strong nuclear force',
+            'Weak nuclear force',
+            'Dark energy',
+        ]
+    },
+       {
+        question: '4. What is the most abundant element in the universe?',
+        answer: 'Hydrogen',
+        options: [
+            'Hydrogen',
+            'Carbon',
+            'Oxygen',
+            'Gold',
+        ]
+    },
+       {
+        question: '5. Which of the following is not a type of black hole?',
+        answer: 'Quasar',
+        options: [
+            'Stellar black hole',
+            'Supermassive black hole',
+            'Microscopic black hole',
+            'Quasar',
+        ]
+    }
+]
+
+// Create text elements and variables
+
+var quiz = document.querySelector("#quiz")
+var timer = document.querySelector("#timer")
+var timer = document.createElement("p")
+var question = document.createElement("p")
+var finalScoreEl = document.createElement("p")
+var answerBtn = document.createElement("button")
+var score
+var finalScore
+var currentQuestion
+var timeLeft
+var timerEl
+
