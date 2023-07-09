48bda85bd356f8b29a7dc954171c9bbb67669306
MindfulOlivka <o.campbell@ufl.edu>
2023-07-09 12:35:49 -0400

    adding click button

--- index.js
+++ index.js
@@ -86,3 +86,30 @@ function startTimer() {
   }, 1000);
 }
 
+
+// if the start button is clicked begin the quiz
+// or if a button within the choices is clicked validate the answer
+// update the score
+// move to the next question
+// end the quiz
+
+quiz.onclick = function (e) {                         // assign function to when click happens
+  if (e.target.id === "start-quiz") {                 // if start quiz clicked trigger function
+    getQuestion();                                    // display the question
+  } else if (
+    e.target.parentElement.id === "choices" &&             // check if parent el  = to our choices
+    e.target.tagName === "BUTTON"                        // check if only btn was clicked
+  ) {
+    if (e.target.textContent === questionsArr[currentQuestion].answer) {    // check if correct answer is clicked
+      score++;                                   // if yes raise the score
+    }
+    clearInterval(timerEl);                      // stop timer
+    currentQuestion++;                                 // move to the next question
+    if (currentQuestion < questionsArr.length) {           // check if this question is not the last one
+      getQuestion();                                   // display next question
+    } else {
+      endQuiz();                                       // end quiz and display final score
+    }
+  }
+};
+
